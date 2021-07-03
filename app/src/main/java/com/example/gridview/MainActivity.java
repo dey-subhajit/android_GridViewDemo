09c013bd6b9b456319eca97992be900df7fd6032
dey-subhajit <subhajit.dey1998@yahoo.com>
2021-07-03 11:19:23 +0530

    Fist commit

---
+++ app/src/main/java/com/example/gridview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.gridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int[] img_arr = {R.drawable.pic1,R.drawable.pic2, R.drawable.pic3,R.drawable.pic4,R.drawable.pic5,R.drawable.pic6,R.drawable.pic7,R.drawable.pic8,R.drawable.pic9,R.drawable.pic10,R.drawable.pic11};
+
+    GridView gv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gv = findViewById(R.id.gv);
+        gv.setAdapter(new ShowImage());
+
+        gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    class ShowImage extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return img_arr.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inf=getLayoutInflater();
+            View row = inf.inflate(R.layout.imageview, null);
+            ImageView iv = row.findViewById(R.id.iv);
+            iv.setImageResource(img_arr[position]);
+            return row;
+        }
+    }
+}
